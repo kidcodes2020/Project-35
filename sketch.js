ab483454bcc6a827bfedfcdda5d003bdc75370e6
kidcodes2020 <74544301+kidcodes2020@users.noreply.github.com>
2021-01-17 19:16:53 +0530

    First

---
+++ sketch.js
@@ -0,0 +1,55 @@
+//Create variables here
+var dog, happyDog, dogImg, happyDogImg;
+var database, foodS, foodStock;
+
+function preload()
+{
+  dogImg=loadImage("images/dogImg.png");
+  happyDogImg=loadImage("images/dogImg1.png");
+}
+
+function setup() {
+  createCanvas(500, 500);
+  dog=createSprite(250,250,20,20);
+  
+  database=firebase.database();
+
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStock);
+
+  dog.addImage(dogImg);
+  dog.scale=0.5;
+}
+
+
+function draw() {  
+  background(rgb(46,139,87));
+  
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foodS);
+    dog.addImage(happyDogImg);
+    dog.scale=0.5;
+  }
+  
+  drawSprites();
+
+  textSize(15);
+  fill("yellow");
+  stroke("yellow");
+  text("Note: Press UP_ARROW Key To Feed Drago Milk!",20,470);
+  //add styles here
+
+}
+
+function readStock(data)
+{
+  foodS=data.val();
+}
+
+function writeStock(x)
+{
+  database.ref('/').update({
+    Food:x
+  })
+}
+
